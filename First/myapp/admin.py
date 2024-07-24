7ff919088acb9ce5ef253b69691b52679f3c6d85
foreverxxxxxx <emineogutlu3@gmail.com>
2024-07-24 15:38:16 +0300

    Django Model Relationships

--- First/myapp/admin.py
+++ First/myapp/admin.py
@@ -1,11 +1,27 @@
+
 from django.contrib import admin
-from .models import Product #First, let's import the product model from models.
+from .models import Category, Product, Supplier ,Address #First, let's import the product model from models.
 
 class ProductAdmin(admin.ModelAdmin):
-    list_display=("name","price","isActive","slug",) #It is used to make it appear column-shaped.
+    list_display=("name","price","isActive","slug","selected_categories",) #It is used to make it appear column-shaped.
     list_display_links=("name","price",)
-    readonly_fields=("slug",)
-    list_filter=("name","price","category",)
+    # readonly_fields=("slug",)
+    prepopulated_fields={"slug":("name",)}
+    list_filter=("name","price","categories")
     list_editable=("isActive",)
     search_fields=("name","description",)
+    def selected_categories(self,obj):
+        html=""
+        for category in obj.categories.all():
+            html +=category.name + " "
+
+        return html
+
+  
+
+
+
 admin.site.register(Product,ProductAdmin) #We can store the model information I have defined in the management panel in this way.
+admin.site.register(Category)
+admin.site.register(Supplier)
+admin.site.register(Address)
